16306c6f70816576ab5bdfbd7ff0460d0c181b4b
tanvibedu <76459290+tanvibedu@users.noreply.github.com>
2021-02-03 11:45:18 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# MakeYourOwnGame4
+# afa156b839e1b81fbfa9bf61b99ed6eb
---
+++ sketch.js
@@ -0,0 +1,59 @@
+//var dog,dogImg,dogImg1;
+var girl,girlImg,girlImg1;
+
+var database;
+
+var foodS,foodStock;
+
+function preload(){
+  girlImg=loadImage("Images/girl.png");
+  //girlImg1=loadImage("Images/happy dog.png");
+}
+
+//Function to set initial environment
+function setup() {
+  //database=firebase.database();
+  
+  createCanvas(500,500);
+
+  girl=createSprite(250,300,150,150);
+  girl.addImage(girlImg);
+  girl.scale=0.15;
+
+  textSize(20); 
+}
+
+// function to display UI
+function draw() {
+  background(46,139,87);
+ 
+  if(keyWentDown(UP_ARROW)){
+    //writeStock(foodS);
+    //dog.addImage(dogImg1);
+    girl.velocityX = -5; 
+  }
+
+  drawSprites();
+  fill(255,255,254);
+  stroke("black");
+  text("Food remaining : "+foodS,170,200);
+  textSize(13);
+  text("Note: Press UP_ARROW Key To Feed Drago Milk!",130,10,300,20);
+}
+
+//Function to read values from DB
+function readStock(data){
+  foodS=data.val();
+}
+
+//Function to write values in DB
+function writeStock(x){
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  } 
+  database.ref('/').update({
+    Food:x
+  })
+}
